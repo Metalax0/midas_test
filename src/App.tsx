fa452a3d9643c9c6f7f4511b8b975301659b7cb8
Metalax <sampannapokharel@gmail.com>
2025-02-12 21:55:14 +0545

    initializing repo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "antd";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="bg-red-500 p-10 rounded-xl">
+            <p>Hello</p> <Button type="primary">PRESS ME</Button>
+        </div>
+    );
+}
+
+export default App;
